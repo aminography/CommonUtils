bd5704d4e8601c3a5a3e43df08dd14610eff5873
Amin <amin.hassani@cafebazaar.cloud>
2020-08-02 13:12:01 +0430

    Some refactoring.

--- library/src/main/java/com/aminography/commonutils/TimeUtils.kt
+++ library/src/main/java/com/aminography/commonutils/TimeUtils.kt
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit
  * @author aminography
  */
 
-val Long.formatDuration: String
+val Long.formatAsDuration: String
     get() {
         fun normalize(number: Long): String = String.format("%02d", number)
 
