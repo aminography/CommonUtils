f26c4152da0a0939beb6e1c87407444e7c910434
Amin <amin.maxim@gmail.com>
2020-08-12 11:05:41 +0430

    Some refactoring.

--- sample-app/src/main/java/com/aminography/commonutils/sample/MainActivity.kt
+++ sample-app/src/main/java/com/aminography/commonutils/sample/MainActivity.kt
@@ -2,6 +2,8 @@ package com.aminography.commonutils.sample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.aminography.commonutils.screenRectDp
+import com.aminography.commonutils.screenRectPx
 
 /**
  * @author aminography
@@ -11,5 +13,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val widthPx = screenRectPx.width()
+        val heightPx = screenRectPx.height()
+        println("[PX] screen width: $widthPx , height: $heightPx")
+
+        val widthDp = screenRectDp.width()
+        val heightDp = screenRectDp.height()
+        println("[DP] screen width: $widthDp , height: $heightDp")
     }
 }
